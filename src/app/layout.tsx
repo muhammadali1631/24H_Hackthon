ae1a930ca2bc4f7d3420fa8f99d5c7056e542c91
Mac <m.alishahzad2000@gmail.com>
2024-12-09 15:57:55 +0500

    solving error4

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import React from 'react'
 import TopBanner from "@/components/TopBanner";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
