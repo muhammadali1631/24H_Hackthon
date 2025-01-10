6482ce83ea150cef18e83b926e128c0e115d8593
Mac <m.alishahzad2000@gmail.com>
2025-01-10 22:42:25 +0500

    update3

--- src/components/NewArrivals.tsx
+++ src/components/NewArrivals.tsx
@@ -1,18 +1,26 @@
+'use client'
 import ProductCard from './ProductCard';
 // import { products } from '@/data/product'
 import ViewAll from './ViewAll';
 import Underline from './Underline';
 import { client } from '@/sanity/lib/client';
 import ProductType from '@/Types/product';
+import { useState } from 'react';
 
-const NewArrivals = async() => {
-  const data:ProductType[] = await client.fetch(`*[_type == 'product'] | order(_createdAt asc)`)
+const NewArrivals = () => {
+  const [data, setData] = useState<ProductType[]>([])
+  const getData = async( )=>{
 
+    const products:ProductType[] = await client.fetch(`*[_type == 'product'] | order(_createdAt asc)`)
+    setData(products)
+  }
+  getData()
+    
   return (
     <div className=" p-4 px-10 my-16" id='new'>
       <h2 className="text-4xl font-black text-center mb-10">NEW ARRIVALS</h2>
       <div className="flex justify-around gap-3 overflow-x-scroll hide-scrollbar my-6">
-        {data.map((product, index) => (
+        {data.filter((item)=> item.tags.includes('new')).map((product, index) => (
           <ProductCard key={index} product={product}/>
         ))}
       </div>
--- src/components/TopSelling.tsx
+++ src/components/TopSelling.tsx
@@ -1,17 +1,24 @@
-import React from 'react'
+'use client'
+import React, { useState } from 'react'
 import ProductCard from './ProductCard'
 import ViewAll from './ViewAll'
 import { client } from '@/sanity/lib/client'
 import ProductType from '@/Types/product'
 
-const TopSelling = async() => {
+const TopSelling = () => {
+  const [data, setData] = useState<ProductType[]>([])
+  const getData = async( )=>{
+
     const products:ProductType[] = await client.fetch(`*[_type == 'product'] | order(_createdAt asc)`)
+    setData(products)
+  }
+  getData()
   
   return (
     <div className=" p-4 px-10 my-16">
       <h2 className="text-4xl font-black text-center mb-10">BEST SELLING</h2>
       <div className="flex justify-around gap-3 overflow-x-scroll hide-scrollbar my-6">
-        {products.filter(item => item.tags.includes('top')).map((product, index) => (
+        {data.filter(item => item.tags.includes('top')).map((product, index) => (
           <ProductCard key={index} product={product}/>
         ))}
       </div>
